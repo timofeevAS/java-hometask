bd5ee36ffc21be732eb5d2a3160d6b2cfa7be729
Alexandr Timofeev <a.timoff3@gmail.com>
2023-10-15 17:06:14 +0300

    Add unit tests,edit reame

---
+++ task1/determinant/README.md
@@ -0,0 +1,27 @@
+# Task 1. Find determinant of Matrix.
+
+## Description
+
+Main task is find determinant of matrix.
+
+For solve this used recursive method and method from `commons-math3` library.
+
+## Requirements
+
+* Apache Maven 3.x+
+* Java 17
+
+## Build and run
+
+```bash
+git clone https://github.com/timofeevAS/java-hometask
+cd java-hometask/task1/determinant
+mvn package
+mvn test # for handly running unit-tests
+```
+
+**To look example use:**
+
+```bash
+mvn exec:java -Dexec.mainClass="ru.spbstu.telematics.java.App"
+```
--- task1/determinant/src/test/java/ru/spbstu/telematics/java/AppTest.java
+++ task1/determinant/src/test/java/ru/spbstu/telematics/java/AppTest.java
@@ -1,38 +1,65 @@
 package ru.spbstu.telematics.java;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+    @Test
+    public void testDeterminantOne(){
+        double data[][] = {{1,2},{3,4}};
+        Matrix matrix = new Matrix(data);
+
+        double determinant = matrix.determinant();
+
+        assertEquals(matrix.apacheDeterminant(), determinant, 0.0001);
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    @Test(expected = IllegalArgumentException.class)
+    public void testMatrixIncorrectSize(){
+        double data[][] = {{1,2,3},{4,5}};
+
+        new Matrix(data);
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test(expected =  IllegalArgumentException.class)
+    public void testDeterminantNotSquareMatrix(){
+        double data[][] = {{1,2,3},{4,5,6}};
+
+        Matrix matrix = new Matrix(data);
+
+        matrix.determinant();
+    }
+
+    @Test
+    public void testBigMatrixDeterminant(){
+
+        final int MAX_DIM_MATRIX = 9;
+
+        for (int i = 1; i <= MAX_DIM_MATRIX; i++) {
+            Matrix m = generateRandomMatrix(i);
+            assertEquals(m.determinant(), m.apacheDeterminant(), 0.0001);
+        }
+    }
+
+
+    private Matrix generateRandomMatrix(int size){
+        double[][] data = new double[size][size]; // create data
+
+        java.util.Random random = new java.util.Random();
+
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data.length; j++) {
+                data[i][j] = random.nextDouble() * 10.0;
+            }
+        }
+
+
+        return new Matrix(data);
     }
 }
